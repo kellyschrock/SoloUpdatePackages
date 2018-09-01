995a31657e0035b5387ab4c3c90784f7fef37ed3
Matt <lawrence.matt@gmail.com>
2018-09-01 11:59:37 -0400

    Controller altitude display fix

---
+++ packages/src/Controller Altitude Fix (AC 3.6+)/scripts/postinstall.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+## Check the files that were uploaded to the /firmware directory.
+packageValid() {
+    if [ -e /firmware/artoo_alt-fix.bin ]
+    then
+        return 0
+    else
+        return 1
+    fi
+}
+
+
+if ! packageValid; then
+    echo "Installation package is corrupt, invalid, or missing. Aborting."
+    exit 1
+else
+    sololink_config --update-apply artoo
+fi
