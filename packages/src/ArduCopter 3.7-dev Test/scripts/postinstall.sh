fc99115338ae8263f600c2166482b0ccf63dc43c
Matt Lawrence <lawrence.matt@gmail.com>
2019-02-11 17:53:56 -0500

    Update to latest Copter 3.7-dev

---
+++ packages/src/ArduCopter 3.7-dev Test/scripts/postinstall.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+set -e
+
+# Set executable permission on each file in this package
+chmod a+x /usr/bin/pixhawk.py
+chmod a+x /usr/bin/uploader.py
+chmod a+x /usr/bin/GoProManager.py
+chmod a+x /usr/bin/modes.py
+chmod a+x /usr/bin/shotManager.py
